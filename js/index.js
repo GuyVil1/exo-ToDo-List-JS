ea5c6c044bbf705da6b9575d729ceca11f0570d8
guy <guy.vilain1}gmail.com>
2020-03-11 22:06:22 +0100

    edition 75% complet

--- js/index.js
+++ js/index.js
@@ -23,37 +23,52 @@ window.onload = () => {
     const todos = [
         {
             text: 'je suis une tâche à accomplir',
-            done: false
+            done: false,
+            editMode: false
         },
         {
             text: 'je suis une seconde tâche à accomplir',
-            done: true
+            done: true,
+            editMode: false
         }
-    ]
+    ];
+
     
     // Les fonctions nécéssaires au bon fonctionnement de mon projet
     
     //Parcourir le tableau todo et pour chaque itération je crée une nouvelle représentation de l'entrée pour la transférer sur le dom
     const displayTodo = () => {
-        const todoNode = todos.map((todo, index) => {
-            return createTodoElement(todo, index);
-        })
+        const todosNode = todos.map((todo, index) => {
+        // Je vérifie si je suis en mode édition ou non et selon, j'affiche ma liste
+        if (todo.editMode){
+            return createTodoEditElement(todo, index);
+            console.log('coucou');
+        }else{
+                return createTodoElement(todo, index);
+            };
+        });
         ul.innerHTML = '';
-        ul.append(...todoNode);
+        ul.append(...todosNode);
     };
     //Fonction pour transformer une todo particulière en élément html
     const createTodoElement = (todo, index) => {
         const li = document.createElement('li');
         const buttonDelete = document.createElement('button');
         buttonDelete.innerHTML =`<i class="fas fa-trash-alt" alt="delete"></i>`;
+        const buttonEdit = document.createElement('button');
+        buttonEdit.innerHTML = `<i class="fas fa-edit"></i>`;
         // Je rajoute un évenement à mon bouton delete
-        buttonDelete.addEventListener('click', () => {
+        buttonDelete.addEventListener('click', event => {
             // Si je clique sur delete, pas de changement toogle possible (la tâche suivante n'est pas validée)
             event.stopPropagation();
             // je me refére à l'index de mon tableau pour savoir quel élément supprimer
             deleteTodo(index);
         });
-
+        // Je rajoute un événement sur mon bouton Edit
+        buttonEdit.addEventListener('click', event => {
+            event.stopPropagation();
+            toggleEditMode(index);
+        });
         // j'ajoute le contenu de ma liste
         li.innerHTML = `
         <span class="todo ${ todo.done ? 'done' : ''  }"></span>
@@ -64,7 +79,22 @@ window.onload = () => {
             toggleTodo(index);
         });
         // je rajoute mon bouton delete
-        li.appendChild(buttonDelete);
+        li.append(buttonEdit, buttonDelete);
+        return li;
+    };
+    // Fonction éditer élément
+    const createTodoEditElement = (todo, index) => {
+        // preparation de mes balises html
+        const li = document.createElement('li');
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.value = todo.text;
+        const buttonSave = document.createElement('button');
+        buttonSave.innerHTML = '<i class="fas fa-save"></i>';
+        const buttonCancel = document.createElement('button');
+        buttonCancel.innerHTML = '<i class="fas fa-window-close"></i>';
+        // Je greffe tout à la liste qui sera créée
+        li.append(input, buttonCancel, buttonSave);
         return li;
     };
     // j'ajoute ma capture à ma liste de todo
@@ -87,6 +117,11 @@ window.onload = () => {
         // si tâche accomplie, j'inverse la valeur actuelle de ma tâche
         todos[index].done = !todos[index].done;
         // je raffraichi ma liste
+        displayTodo();
+    };
+    // Fonction éditer tâche
+    const toggleEditMode = (index) =>{
+        todos[index].editMode = !todos[index].editMode;
         displayTodo();
     };
     displayTodo();
